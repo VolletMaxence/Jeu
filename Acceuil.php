02f610fd95b6d31a7eb53f191fc540aab5e1f66c
VOLLETMaxence <mvollet.la-providence.net>
2021-03-11 15:59:03 +0100

    Changement CSS pour tableau Acceuil

--- Acceuil.php
+++ Acceuil.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8">
         <title>Salon</title>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="CSS/Accueil.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="CSS/Index.css" media="screen" type="text/css" />
     </head>
     <body>
         <?php
@@ -54,32 +54,33 @@
                 //$BDD = new PDO("mysql:host=192.168.64.116; dbname=Maxence_Jeu; charset=utf8", "root", "root");
                 //base Alays Data :
                 $BDD = new PDO("mysql:host=mysql-xence.alwaysdata.net; dbname=xence_maxence_jeu; charset=utf8", "xence", "Tallys2001");
+
                 //Affichage des 6 meilleurs
                 $CommResult = $BDD->query("SELECT `Nom`, `Score` FROM `perso` ORDER BY `Score` DESC LIMIT 6");
                 // selectionne le id du jeux le nom et sa fait une jointure avec articlelike et game
                 ?>
-                    <div id="text">
-                        <h1>Tableau des scores</h1>
-                    </div>
-                        <table id="Score">
-                            <?php
-                            While($don = $CommResult->fetch()){   // boucle while qui affiche les 3 meuilleur jeux
-                                ?>        
-                                    <td>
-                                        <?php
-                                        echo '<h1 class="align">'.$don['Nom'].'</h1>'; //affiche le nom du jeu
-                                        ?>
-                                    </td>
-                                    <td>
-                                        <?php
-                                        echo '<h1 class="align">'.$don['Score'].'</h1>'; //affiche le mombre de coeur
-                                        ?>
-                                    </td>
-                                </tr>
+                   <div id="text">
+                    <h1>Tableau des scores</h1>
+                </div>
+                    <table id="Score">
+                        <?php
+                        While($don = $CommResult->fetch()){   // boucle while qui affiche les 3 meuilleur jeux
+                            ?>        
+                                <td>
+                                    <?php
+                                    echo '<h1 class="align">'.$don['Nom'].'</h1>'; //affiche le nom du jeu
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    echo '<h1 class="align">'.$don['Score'].'</h1>'; //affiche le mombre de coeur
+                                    ?>
+                                </td>
+                            </tr>
                             <?php
                         }
-                        ?>
-                    </table>
+                    ?>
+                </table>
                 <?php
             }
         ?>
